9e96ba0497282ac2c9cc75010faed4256222b984
Neest <denyska7@yandex.ru>
2016-11-23 21:21:17 +0200

    ADD: can build area charts based on array of tasks, chart shows count of tasks that was completed on each of several recent days

--- demo.js
+++ demo.js
@@ -48,97 +48,10 @@ let data = [
 /*********************************
 * TEST CALL
 *********************************/
-chart(data, {
+Chart.areaChart(data, {
   selector: '#paper',
   height: 250,
   width: 350,
-  period: 30
+  period: 30 
 });
-
-
-/*********************************
-* CHART
-*********************************/
-function chart(data, ops) {
-  if(!ops) return false;
-
-  let paper = Snap(ops.selector).attr({
-    height: ops.height,
-    width: ops.width,
-    viewBox: `0 -${ops.height-5} ${ops.width} ${ops.height}`,
-  });
-
-  let columns = getColumnsData(),
-  colWidth = ops.width / ops.period,
-  radius = offsetX = 1,
-  scale = 10;
-
-  let path = paper.path().attr({
-    stroke: '#1D7988',
-    fill: '#33A6B8',
-    strokeWidth: '1px',
-  });
-
-  for(let col of columns) {
-    // if(col.count)
-    //   paper.circle(offsetX, -col.count * scale, radius).attr({
-    //     fill: '#fff',
-    //   }); 
-
-    let pathString = path.attr('d');
-    let coords = `${offsetX},${-col.count * scale}`;
-    let startPos = `M ${ops.width-offsetX},0 L 0,0`;
-
-    path.attr({
-      // d: pathString ? pathString + `L ${coords}` : `M ${coords}`,
-      d: pathString ? pathString + `L ${coords}` : startPos + `L ${coords}`,
-    });
-    offsetX += colWidth;
-  }
-
-  path.attr({ d: path.attr('d') + 'Z' })
-
-  function getColumnsData() {
-    const period = ops.period || 30,
-      daysInMonth = function(year, month) {
-        return 32 - new Date(year, month, 32).getDate();
-      };
-
-    let curDate = new Date(),
-      columns = [],
-      curDay = curDate.getDate(),
-      curMonth = curDate.getMonth(),
-      curYear = curDate.getFullYear();
-
-    /*************************************************
-    * CREATE LIST OF CHART COLUMNS
-    *************************************************/
-    for(let i = 0; i < period; i++) {
-      if(curDay == 0){
-        curDay = daysInMonth(curYear, --curMonth);
-      }
-
-      columns.push({
-        date: new Date(curYear, curMonth, curDay--).getTime(),
-        count: 0,
-        tasks: [],
-      });
-    }
-
-    /*************************************************
-    * CREATE CHART POINTS
-    *************************************************/
-    for(let task of data) {
-      let taskTime = task.date.setHours(0,0,0,0);
-
-      for(let col of columns)
-        if(col.date == taskTime) {
-          col.count++;
-          col.tasks.push(task);
-        }
-    }
-
-    return columns.reverse();
-  }
-};
 
