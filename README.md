27debbf3e398d47015091bebee572149a1e5da5c
Neest <denyska7@yandex.ru>
2016-11-23 20:35:26 +0200

    REFACTOR: from class to plain object. It makes possible the extention

--- README.md
+++ README.md
@@ -10,9 +10,19 @@ All you need is to add javascript file to you html page
 
 ![image](https://pp.vk.me/c636629/v636629817/31a60/zyd6A7J5ClM.jpg)
 
-And create diagram using methods of Chart class. The class takes an object to it's contructor. This object is supposed to contain css and svg properties in order to customize your diagram. If you want to tune it using methods, you can leave the settings object empty.
-
-![image](https://pp.vk.me/c636629/v636629817/31a59/XP1FcsWTq-Q.jpg)
+```javascript
+Chart.radialChart({
+  parent: '#first',
+  persent: 70, 
+  r: 100,
+  width: 15,
+  duration: 700,
+  strokeFilled: '#00c8ff',
+  strokeEmpty: 'transparent',
+  fontFamily: 'Poiret One',
+  fontWeight: 'bold'
+});
+```
 
 ## Requirements
 Snap.svg
--- chart.js
+++ chart.js
@@ -1,200 +1,119 @@
-class Chart {
-  constructor(args) {
+window.Chart = {
+
+  radialChart: function(args) {
     if(args.persent > 100) args.persent = 100;
     if(args.persent < 0) args.persent = 0;
 
-    this._persent = args.persent;
-    this._r = args.r;
-    this._width = args.width;
-    this._parent = Snap.select(args.parent);
-    this._strokeFilled = args.strokeFilled || '#3d3d3d';
-    this._strokeEmpty = args.strokeEmpty || args.fill || '#eee';
-    this._fill = args.fill || args.strokeEmpty || 'transparent';
-    this._fontClass = args.fontClass;
-    this._fontColor = args.fontColor || args.strokeFilled;
-  }
+    /********************************************
+    * INIT
+    ********************************************/
+    let persent = args.persent,
+      r = args.r,
+      width = args.width,
+      parent = Snap.select(args.parent),
+      strokeFilled = args.strokeFilled || '#3d3d3d',
+      strokeEmpty = args.strokeEmpty || args.fill || '#eee',
+      fill = args.fill || args.strokeEmpty || 'transparent',
+      fontClass = args.fontClass,
+      fontColor = args.fontColor || args.strokeFilled,
+      fontFamily = args.fontFamily,
+      fontWeight = args.fontWeight,
+      duration = args.duration;
 
-  /*****************************
-  /* Private
-  /****************************/
+      let path;
 
-  _polarToCartesian(cx, cy, r, angle) {
-    angle = (angle - 90) * Math.PI / 180;
-    return {
-      x: cx + r * Math.cos(angle),
-      y: cy + r * Math.sin(angle)
-    };
-  }
-  _describeArc(x, y, r, startAngle, endAngle) {
-    startAngle = startAngle % 360
-    let start = this._polarToCartesian(x, y, r, startAngle);
-    let end = this._polarToCartesian(x, y, r, endAngle);
-    let large = Math.abs(endAngle - startAngle) >= 180;
-    return `M${start.x},${start.y} A${r},${r},0,${large ? 1 : 0}1,${end.x},${end.y}`;
-  }
+    /********************************************
+    * PRIVATE
+    ********************************************/
 
-  _updatePath(coords, radius, persents) {
-    let deg = persents * 359.9999 / 100;
-    this._path.attr({
-      d: this._describeArc(coords.x,coords.y, radius, 0, deg)
-    });
-  }
-
-  /*****************************
-  /* Public
-  /****************************/
-
-  parent(selector) {
-    this._parent = Snap.select(selector);
-    return this;
-  }
-
-  persent(persent) {
-    if(persent > 100) persent = 100;
-    if(persent < 0) persent = 0;
-
-    this._persent = persent;
-    return this;
-  }
-
-  r(radius) {
-    this._r = radius;
-    return this;
-  }
-
-  strokeFilled(color) {
-    this._strokeFilled = color;
-    this._fontColor = color;
-    return this;
-  }
-
-  strokeEmpty(color) {
-    this._strokeEmpty = color;
-    return this;
-  }
-
-  fill(color) {
-    this._fill = color;
-    return this;
-  }
-
-  width(width) {
-    this._width = width;
-    return this;
-  }
-
-  fontColor(color) {
-    this._fontColor = color;
-    return this;
-  }
-
-  fontFamily(font) {
-    this._fontFamily = font;
-    return this;
-  }
-
-  fontWeight(weight) {
-    this._fontWeight = weight;
-    return this;
-  }
-
-  draw(duration) {
-    this._sector = Snap(this._parent);
-
-    this._circle = this._sector
-    .circle(this._r + this._width, this._r + this._width, this._r)
-    .attr({
-      fill: this._fill,
-      stroke: this._strokeEmpty,
-      strokeWidth: this._width
-    });
-
-    this._path = this._sector.path('').attr({
-      fill: 'transparent',
-      stroke: this._strokeFilled,
-      strokeWidth: this._width
-    });
-
-    let persent = 1;
-
-    let t = this._sector.text(0, 0, persent < 10 ? '0%' : '00%')
-    .attr({
-      fontSize: this._r / 2.7 + 'px',
-      textAnchor: 'middle',
-      fill: this._fontColor,
-      fontFamily: this._fontFamily,
-      fontWeight: this._fontWeight
-    })
-    .addClass(this._fontClass);
-    let textParams = t.getBBox();
-
-    t.attr({
-      y: (this._r + this._width) + textParams.h / 2,
-      x: (this._r + this._width),
-      text: 75 + '%',
-    });
-    let coords = {
-      x: this._r + this._width,
-      y: this._r + this._width
+    function polarToCartesian(cx, cy, r, angle) {
+      angle = (angle - 90) * Math.PI / 180;
+      return {
+        x: cx + r * Math.cos(angle),
+        y: cy + r * Math.sin(angle)
+      };
     }
 
-    let timer = setInterval(() => {
-      if(persent >= this._persent) clearInterval(timer);
-      this._updatePath(coords, this._r, persent);
-      t.attr({text: persent + '%'})
+    function describeArc(x, y, r, startAngle, endAngle) {
+      startAngle = startAngle % 360;
+      let start = polarToCartesian(x, y, r, startAngle),
+          end = polarToCartesian(x, y, r, endAngle),
+          large = Math.abs(endAngle - startAngle) >= 180;
+      return `M${start.x},${start.y} A${r},${r},0,${large ? 1 : 0}1,${end.x},${end.y}`;
+    }
 
-      persent++;
-    }, duration / this._persent);
+    function updatePath(coords, radius, persents) {
+      let deg = persents * 359.9999 / 100;
+      path.attr({
+        d: describeArc(coords.x, coords.y, radius, 0, deg)
+      });
+    }
 
-    this._path.attr({strokeWidth: this._width || 10})
+    function draw(duration) {
+      const sector = Snap(parent);
+
+      let circle = sector
+      .circle(r + width, r + width, r)
+      .attr({
+        fill: fill,
+        stroke: strokeEmpty,
+        strokeWidth: width
+      });
+
+      path = sector.path('').attr({
+        fill: 'transparent',
+        stroke: strokeFilled,
+        strokeWidth: width
+      });
+
+      let currentPersent = 1;
+
+      let t = sector.text(0, 0, currentPersent < 10 ? '0%' : '00%')
+      .attr({
+        fontSize: r / 2.7 + 'px',
+        textAnchor: 'middle',
+        fill: fontColor,
+        fontFamily: fontFamily,
+        fontWeight: fontWeight
+      })
+      .addClass(fontClass);
+
+      t.attr({
+        y: (r + width) + t.getBBox().h / 2,
+        x: (r + width),
+        text: 75 + '%',
+      });
+
+      let coords = {
+        x: r + width,
+        y: r + width
+      }
+
+      let timer = setInterval(() => {
+        if(currentPersent >= persent) clearInterval(timer);
+        updatePath(coords, r, currentPersent);
+        t.attr({text: currentPersent + '%'})
+
+        currentPersent++;
+      }, duration / persent);
+
+      path.attr({strokeWidth: width || 10})
+    }
+
+    draw(duration);
+
   }
+
 }
 
-
-/*****************************
-/* Test
-/****************************/
-
-let settings = {
+Chart.radialChart({
+  parent: '#first',
+  persent: 70, 
   r: 100,
   width: 15,
-  strokeFilled: '#90be8c',
-  strokeEmpty: '#fff',
-}
-
-let a = new Chart(settings)
-  .parent('#first')
-  .persent(70)
-  .width(11)
-  .r(110)
-  .fill('transparent')
-  .strokeEmpty('transparent')
-  .fontFamily('Poiret One')
-  .fontWeight('bold')
-  .strokeFilled('#00c8ff')
-  .draw(300);
-
-
-let b = new Chart(settings)
-  .parent('#second')
-  .persent(60)
-  .r(110)
-  .strokeFilled('#53d260')
-  .strokeEmpty('transparent')
-  .fill('#eee')
-  .fontColor('#3d3d3d')
-  .fontFamily('PT Sans')
-  .draw(700);
-
-let c = new Chart(settings)
-  .parent('#third')
-  .persent(43)
-  .strokeFilled('#ff6c6c')
-  .strokeEmpty('#535353')
-  .fill('transparent')
-  .fontFamily('Open Sans')
-  .fontWeight('bold')
-  .fontColor('#fff')
-  .r(110)
-  .width(8)
-  .draw(700);
+  duration: 700,
+  strokeFilled: '#00c8ff',
+  strokeEmpty: 'transparent',
+  fontFamily: 'Poiret One',
+  fontWeight: 'bold'
+});
